ab3da0b92c288d1f8ae2ef281681abc8d13a9a6e
gq <91thenx@gmail.com>
2020-08-19 01:29:44 +0800

    use bootstrap

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 from flask import request
-
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+bootstrap = Bootstrap(app)  # init bootstrap
 
 
 @app.route('/')
@@ -25,6 +26,16 @@ def index():
 def user(name):
     """渲染模板"""
     return render_template("user.html", name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
 
 
 if __name__ == '__main__':
